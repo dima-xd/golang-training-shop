070f6e6f0e6cbdb91f8754093863e6ac8c1529f9
Dzmitry Dubrova <dimaxdqwerty@gmail.com>
2021-04-20 15:02:37 +0300

    some fixes

--- task_3/pkg/data/query.go
+++
@@ -1,7 +0,0 @@
-package data
-
-const selectMainOrderInfo = `orders.id, customers.name AS customer_name, customers.surname, 
-	products.name AS product_name, customers.contact, date, price`
-const joinCustomers = `JOIN customers ON customers.id = customer_id`
-const joinProducts = `JOIN products ON products.id = product_id`
-
