82254533c8b4c1dea9acb38cc39b5f58ab7715d2
Dzmitry Dubrova <dimaxdqwerty@gmail.com>
2021-04-30 14:31:40 +0300

    minor fix

--- task_3/pkg/data/query.go
+++ task_3/pkg/data/query.go
@@ -1,7 +1,8 @@
 package data
 
-const selectMainOrderInfo = `orders.id, customers.name AS customer_name, customers.surname, 
-	products.name AS product_name, customers.contact, date, price`
-const joinCustomers = `JOIN customers ON customers.id = customer_id`
-const joinProducts = `JOIN products ON products.id = product_id`
-
+const (
+	selectMainOrderInfo = `orders.id, customers.name AS customer_name, customers.surname,
+		products.name AS product_name, customers.contact, date, price`
+	joinCustomers = `JOIN customers ON customers.id = customer_id`
+	joinProducts  = `JOIN products ON products.id = product_id`
+)
